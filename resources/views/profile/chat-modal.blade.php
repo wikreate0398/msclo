71d80ec85e4398f0136f84254f73da26b9cf2e65
Maxim Paladi <paladium98@gmail.com>
2020-09-01 13:18:09 +0300

    [1] add test chat modal

---
+++ resources/views/profile/chat-modal.blade.php
@@ -0,0 +1,21 @@
+
+
+<div class="modal fade" id="chatModal" tabindex="-1" role="dialog" aria-labelledby="chatModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+       <div class="modal-content">
+          <div class="modal-header">
+             <h5 class="modal-title" id="chatModalLabel">Тестовое модальное окно</h5>
+             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+             </button>
+          </div>
+          <div class="modal-body">
+             <p class="modal-text"> Тестовое модальное окно. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum numquam quidem accusamus voluptatibus reiciendis corporis molestias ex sunt voluptate. Molestias modi necessitatibus voluptatem molestiae totam facere earum reprehenderit quibusdam cumque? </p>
+          </div>
+          <div class="modal-footer">
+            <button id="btn-add" class="btn">Добавить</button>
+             <button class="btn" data-dismiss="modal"><i class="flaticon-cancel-12"></i> Отмена</button>
+          </div>
+       </div>
+    </div>
+ </div>

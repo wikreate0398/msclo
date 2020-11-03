9916f39feda5168267d066fa9447e118ea643a89
wikreate <fleancu.daniel@gmail.com>
2020-11-03 16:19:09 +0200

    fix issue

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -6,7 +6,9 @@
 
     <!-- Required Meta Tags Always Come First -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">--}}
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate, private" />
     <meta http-equiv="Pragma" content="no-cache" />
